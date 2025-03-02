66d54541cd80c0572598186f5330080f5e4f4f14
hirwa cedric <hirwacedr12@gmail.com>
2025-03-02 11:16:00 +0200

    add home view and template with URL routing

--- AllauthProj/AllauthProj/settings.py
+++ AllauthProj/AllauthProj/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'AllauthProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
